ee5f4da170d4946836a808a2bc3c3ec8923c49cf
Aleksander Niedzwiedz <niedzwiedz.aleksander@gmail.com>
2021-08-08 16:08:52 +0200

    chore: change app html structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
-import { FC } from 'react';
+import { FC } from "react";
 import { Chat } from "./chat";
 
 const App: FC = () => {
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <Chat />
-      </header>
-    </div>
+    <main>
+      <Chat />
+    </main>
   );
-}
+};
 
 export default App;
